1bea3258f2d4e0f51c59a4d41853e38551636e34
Saša Barišić <cartman300@net.hr>
2017-02-21 18:05:43 +0100

    Added loading of already loaded modules :joy:
    Resharper. Now.

--- GSharp/InterfaceLoader.cs
+++ GSharp/InterfaceLoader.cs
@@ -3,52 +3,59 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
-namespace GSharp
-{
-    public static class InterfaceLoader
-    {
-        [DllImport("Kernel32.dll", EntryPoint = "LoadLibrary")]
-        private static extern IntPtr LoadLibraryInternal(string path);
+namespace GSharp {
+	public static class InterfaceLoader {
+		[DllImport("kernel32", EntryPoint = "LoadLibrary")]
+		private static extern IntPtr LoadLibraryInternal(string path);
 
-        [DllImport("Kernel32.dll")]
-        private static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+		[DllImport("kernel32")]
+		private static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
 
-        private static Dictionary<string, IntPtr> LibraryCache = new Dictionary<string, IntPtr>();
+		[DllImport("kernel32")]
+		private static extern IntPtr GetModuleHandle(string path);
 
-        private static IntPtr LoadLibrary(string path)
-        {
-            if (LibraryCache.ContainsKey(path)) return LibraryCache[path];
-            var handle = LoadLibraryInternal(path);
-            LibraryCache.Add(path, handle);
-            return handle;
-        }
+		private static Dictionary<string, IntPtr> LibraryCache = new Dictionary<string, IntPtr>();
 
-        private static CreateInterfaceFn LoadCreateInterface(string dllPath)
-        {
-            var hModule = LoadLibrary(dllPath);
-            var functionAddress = GetProcAddress(hModule, "CreateInterface");
-            return (CreateInterfaceFn)Marshal.GetDelegateForFunctionPointer(functionAddress, typeof(CreateInterfaceFn));
-        }
+		private static IntPtr LoadLibrary(string path)
+		{
+			if (LibraryCache.ContainsKey(path)) return LibraryCache[path];
 
-        public static TClass Load<TClass>(string dllname) where TClass : class
-        {
+			IntPtr handle = GetModuleHandle(path);
+			if (handle == IntPtr.Zero)
+				handle = LoadLibraryInternal(path);
+			if (handle == IntPtr.Zero)
+				return IntPtr.Zero;
 
-            var CreateInterface = LoadCreateInterface(dllname);
+			LibraryCache.Add(path, handle);
+			return handle;
+		}
 
-            IntPtr address = CreateInterface(InterfaceVersions.GetInterfaceIdentifier(typeof(TClass)), IntPtr.Zero);
-            if (address == IntPtr.Zero)
-                return default(TClass);
+		private static CreateInterfaceFn LoadCreateInterface(string dllPath)
+		{
+			var hModule = LoadLibrary(dllPath);
+			var functionAddress = GetProcAddress(hModule, "CreateInterface");
+			return (CreateInterfaceFn)Marshal.GetDelegateForFunctionPointer(functionAddress, typeof(CreateInterfaceFn));
+		}
 
-            var ret = JITEngine.GenerateClass<TClass>(address);
-            return ret;
-        }
+		public static TClass Load<TClass>(string dllname) where TClass : class
+		{
 
-        public static IntPtr LoadVariable(string dllname, string variableName)
-        {
-            var handle = LoadLibrary(dllname);
-            var varHandle = GetProcAddress(handle, variableName);
-            return varHandle;
-            
-        }
-    }
+			var CreateInterface = LoadCreateInterface(dllname);
+
+			IntPtr address = CreateInterface(InterfaceVersions.GetInterfaceIdentifier(typeof(TClass)), IntPtr.Zero);
+			if (address == IntPtr.Zero)
+				return default(TClass);
+
+			var ret = JITEngine.GenerateClass<TClass>(address);
+			return ret;
+		}
+
+		public static IntPtr LoadVariable(string dllname, string variableName) 
+		{
+			var handle = LoadLibrary(dllname);
+			var varHandle = GetProcAddress(handle, variableName);
+			return varHandle;
+
+		}
+	}
 }
