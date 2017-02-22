d32d9b59f7000a9985b3918ae17b96d9f16ff0e4
DuckyC <me@magnushj.dk>
2017-02-22 14:54:55 +0100

    Added rebracer file

--- dotnet/Module.cs
+++ dotnet/Module.cs
@@ -24,11 +24,11 @@ namespace dotnet
         [DllExport("gmod13_open", CallingConvention = CallingConvention.Cdecl)]
         public static int Open(lua_state L)
         {
-			//VCR_t* VCR = (VCR_t*)InterfaceLoader.LoadVariable<VCR_t>("tier0.dll", "g_pVCR");
-			
-			//OHook_recvfrom = InterfaceLoader.OverwriteVCRHook(VCR, new_Hook_recvfrom = Hook_recvfrom_detour);
-			//old_Hook_Cmd_Exec = InterfaceLoader.OverwriteVCRHook(VCR, new_Hook_Cmd_Exec = Hook_Cmd_Exec);
-			
+            //VCR_t* VCR = (VCR_t*)InterfaceLoader.LoadVariable<VCR_t>("tier0.dll", "g_pVCR");
+
+            //OHook_recvfrom = InterfaceLoader.OverwriteVCRHook(VCR, new_Hook_recvfrom = Hook_recvfrom_detour);
+            //old_Hook_Cmd_Exec = InterfaceLoader.OverwriteVCRHook(VCR, new_Hook_Cmd_Exec = Hook_Cmd_Exec);
+
 
 
             var netsockptr = SymbolFinder.ResolveOnBinary("engine.dll", net_sockets_sig);
@@ -62,11 +62,12 @@ namespace dotnet
             return OHook_recvfrom(s, buf, len, flags, from, fromlen);
         }
 
-		static Hook_Cmd_Exec new_Hook_Cmd_Exec;
-		static Hook_Cmd_Exec old_Hook_Cmd_Exec;
-		public static void Hook_Cmd_Exec(string[] Args) {
-			old_Hook_Cmd_Exec(Args);
-		}
+        static Hook_Cmd_Exec new_Hook_Cmd_Exec;
+        static Hook_Cmd_Exec old_Hook_Cmd_Exec;
+        public static void Hook_Cmd_Exec(string[] Args)
+        {
+            old_Hook_Cmd_Exec(Args);
+        }
 
         [DllExport("gmod13_close", CallingConvention = CallingConvention.Cdecl)]
         public static int Close(IntPtr L)
